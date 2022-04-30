6cfb0320aca382100fa5b2a61af651299dad5877
atif9 <mohammadatif92080@gmail.com>
2022-04-30 16:00:45 +0530

    editText finished typing working

--- app/src/main/java/com/example/youtubesearcher/MainActivity.kt
+++ app/src/main/java/com/example/youtubesearcher/MainActivity.kt
@@ -2,16 +2,46 @@ package com.example.youtubesearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
 import com.example.youtubesearcher.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    val DELAY:Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // checking the auto event trigger in edittext
 
+        var timer = Timer()
+
+        binding.txtSearch.addTextChangedListener (
+            object:TextWatcher{
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                }
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    timer.cancel()
+                    timer.purge()
+
+                }
+                override fun afterTextChanged(p0: Editable?) {
+                    timer = Timer()
+                    timer.schedule(object : TimerTask() {
+                        override fun run() {
+                            Log.d("check","working")
+                        }
+                    }, DELAY)
+                }
+            }
+        )
     }
 }
